ed51f311e5383c0c36372d7c38d0a7b8e98d5590
Joel T <joel.thiessen@automattic.com>
2022-04-27 13:58:14 -0700

    Enabling reminder bar for both experiments and control

--- plugins/woocommerce-admin/client/tasks/reminder-bar/reminder-bar.tsx
+++ plugins/woocommerce-admin/client/tasks/reminder-bar/reminder-bar.tsx
@@ -28,7 +28,6 @@ import './reminder-bar.scss';
 
 type ReminderBarProps = {
 	taskListId: string;
-	pageTitle: string;
 	updateBodyMargin: () => void;
 };
 
@@ -87,7 +86,7 @@ const ReminderText: React.FC< ReminderTextProps > = ( {
 };
 
 export const TasksReminderBar: React.FC< ReminderBarProps > = ( {
-	taskListId = 'setup_experiment_1',
+	taskListId,
 	updateBodyMargin,
 } ) => {
 	const { updateOptions } = useDispatch( OPTIONS_STORE_NAME );
