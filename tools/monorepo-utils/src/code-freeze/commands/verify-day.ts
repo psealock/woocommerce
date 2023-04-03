1a57f9df6bc4f6f07d6a53dbf134ec402b0d0c22
Paul Sealock <psealock@gmail.com>
2023-04-03 13:20:14 +1200

    get hello world working

--- tools/monorepo-utils/src/code-freeze/commands/verify-day.ts
+++ tools/monorepo-utils/src/code-freeze/commands/verify-day.ts
@@ -6,7 +6,12 @@ import { Command } from '@commander-js/extra-typings';
 /**
  * Internal dependencies
  */
-import { verifyDay } from '../utils/index';
+import {
+	isTodayCodeFreezeDay,
+	DAYS_BETWEEN_CODE_FREEZE_AND_RELEASE,
+	getToday,
+	getFutureDate,
+} from '../utils/index';
 
 export const verifyDayCommand = new Command( 'verify-day' )
 	.description( 'Verify if today is the code freeze day' )
@@ -14,8 +19,34 @@ export const verifyDayCommand = new Command( 'verify-day' )
 		'-o, --override <override>',
 		"Time Override: The time to use in checking whether the action should run (default: 'now')."
 	)
-	.action( () => {
-		console.log( verifyDay() );
+	.action( ( { override } ) => {
+		const today = getToday( override );
+		const futureDate = getFutureDate( today );
+		console.log(
+			chalk.yellow(
+				"Today's timestamp UTC is: " + today.toUTCString() + '\n'
+			)
+		);
+		console.log(
+			chalk.yellow(
+				`Checking to see if ${ DAYS_BETWEEN_CODE_FREEZE_AND_RELEASE } days from today is the second Tuesday of the month.\n`
+			)
+		);
+		const isCodeFreezeDay = isTodayCodeFreezeDay( override );
+		console.log(
+			chalk.green(
+				`${ futureDate.toUTCString() } ${
+					isCodeFreezeDay ? 'is' : 'is not'
+				} release day.\n`
+			)
+		);
+		console.log(
+			chalk.green(
+				`Today is ${
+					isCodeFreezeDay ? 'indeed' : 'not'
+				} code freeze day.`
+			)
+		);
 
 		process.exit( 0 );
 	} );
