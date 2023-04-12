9123a92f72c78eab55a508114315bdc0a5e9f14d
Paul Sealock <psealock@gmail.com>
2023-04-12 14:04:35 +1200

    try adding command to action

--- tools/monorepo-utils/src/index.ts
+++ tools/monorepo-utils/src/index.ts
@@ -8,6 +8,7 @@ import { Command } from '@commander-js/extra-typings';
  * Internal dependencies
  */
 import CodeFreeze from './code-freeze/commands';
+export { isTodayCodeFreezeDay } from './code-freeze/utils';
 
 const program = new Command()
 	.name( 'utils' )
