1df38ab7553daa42bb08a872cb7149a98409859a
paul sealock <psealock@gmail.com>
2023-07-14 16:57:45 +1200

    Prep test/release/99 for 11.0 cycle with version bump to 11.0.0-a.4

--- plugins/woocommerce/woocommerce.php
+++ plugins/woocommerce/woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce
  * Plugin URI: https://woocommerce.com/
  * Description: An eCommerce toolkit that helps you sell anything. Beautifully.
- * Version: 11.0.0-a.3
+ * Version: 11.0.0-a.4
  * Author: Automattic
  * Author URI: https://woocommerce.com
  * Text Domain: woocommerce
